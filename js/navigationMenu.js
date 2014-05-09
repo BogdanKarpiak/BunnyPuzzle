feff0afe338215ed54d9fcf4f5c5a6c45118a714
BohdanKarpiak <bogdan.karpiak@gmail.com>
2014-05-09 17:11:55 +0300

    fdg

--- js/navigationMenu.js
+++ js/navigationMenu.js
@@ -8,105 +8,162 @@ menuConfig = function() {
     var isShownMenu = 0;
     var menu = {
         init: function() {
-            this.$mainMenu = $("#menuButton");
-            this.$menuPanel = $("#menuPanel");
-            this.buttons = $("#buttons");
-            this.backButton = $("#back");
-            this.reloadButton = $("#reload");
-            this.messageWindow = $("#messageWindow");
-            this.okButton = $("#okButton");
-            this.cancelButton = $("#cancelButton");
-            var windowWidth = 50 * screenWidth / 100;
-            var windowHeight = menu.messageWindow.height() * windowWidth/menu.messageWindow.width();
-            var messageWindowPosition = {
-                top: screenHeight/2 - menu.messageWindow.height()/2,
-                left: screenWidth/2 - windowWidth/2
-            };
-
-            this.$mainMenu.show();
-            this.$mainMenu.css({
-                width: menuSize,
-                top: panelWidth/2 - menuSize/2,
-                left: panelWidth/2 - menuSize/2
-            });
-            this.$menuPanel.css({
-                width: panelWidth + "px",
-                height: screenHeight,
-                marginLeft: - panelWidth + "px"
-            });
-            this.buttons.find(".navButton").css({
-                width: buttonSize + "px",
-                marginBottom: 30 * panelWidth / 100 + "px"
-            }).show();
-            this.buttons.css({
-                marginLeft: - panelWidth + "px",
-                marginTop: - screenHeight + (panelWidth/2 - menuSize/2) + menuSize * 1.4 + "px"
-            });
-            menu.messageWindow.css({
-                top: messageWindowPosition.top,
-                left: messageWindowPosition.left,
-                width: windowWidth
-            });
-            menu.okButton.css({
-                position: "absolute",
-                top: messageWindowPosition.top + 50 * windowHeight / 100 + "px",
-                left: messageWindowPosition.left + 15 * windowWidth / 100 + "px",
-                width: 30 * windowWidth / 100,
-                marginRight: 10 * windowWidth / 100 + "px"
-            });
-            menu.cancelButton.css({
-                position: "absolute",
-                top: messageWindowPosition.top + 50 * windowHeight / 100 + "px",
-                left: messageWindowPosition.left + 55 * windowWidth / 100 + "px",
-                width: 30 * windowWidth / 100 + "px"
-            });
-            //////////////////////////////////////////////////////////////////////
-            menu.$mainMenu.on(isMobile ? "touchend" : "click", function() {
-                if(isShownMenu) {
-                    menu.hide();
-                } else {
-                    menu.show();
+            function loadImages(imgSources, callback) {
+                var assetDir = '../images/menu/';
+                var images = {};
+                var loadedImages = 0;
+                var numImages = 0;
+                for(var src in imgSources) {
+                    numImages++;
                 }
-            });
-            menu.backButton.on(isMobile ? "touchend" : "click", function() {
-                if($(document.body).attr("class") == "getPuzzle") {
-                    document.location.href = localStorage.getItem('environment') + "Environment.html";
-                } else {
-                    document.location.href = "environmentChoose.html";
+                for(var src1 in imgSources) {
+                    menu[src1] = $("#" + src1);
+                    console.log(menu[src1]);
+                    menu[src1].on("load", function() {
+                        if(++loadedImages >= numImages) {
+                            callback();
+                        }
+                    });
+                    menu[src1].attr("src", "" + assetDir + imgSources[src1]);
                 }
-            });
-            menu.okButton.on(isMobile ? "touchend" : "click", function() {
-                document.location.href = document.location.href;
-            });
-            menu.cancelButton.on(isMobile ? "touchend" : "click", function() {
-                menu.messageWindow.hide();
-                menu.okButton.hide();
-                menu.cancelButton.hide();
-            });
-            menu.reloadButton.on(isMobile ? "touchend" : "click", function() {
+            }
+            function initImagesBehavior() {
+                menu.$menuPanel = $("#menuPanel");
+                menu.buttons = $("#buttons");
+                /*menu.$mainMenu = $("#mainMenu");
+                menu.backButton = $("#back");
+                menu.reloadButton = $("#reload");
+                menu.messageWindow = $("#messageWindow");
+                menu.okButton = $("#okButton");
+                menu.cancelButton = $("#cancelButton");*/
 
-                $("<div>").css({
+                /*menu.$mainMenu = $(images["mainMenu"]);
+                menu.backButton = $(images["backButton"]);
+                menu.reloadButton = $(images["reloadButton"]);
+                menu.messageWindow = $(images["messageWindow"]);
+                menu.okButton = $(images["okButton"]);
+                menu.cancelButton = $(images["cancelButton"]);*/
+
+                var windowWidth = 50 * screenWidth / 100;
+                var windowHeight = menu.messageWindow.height() * windowWidth/menu.messageWindow.width();
+                var messageWindowPosition = {
+                    top: screenHeight/2 - menu.messageWindow.height()/2,
+                    left: screenWidth/2 - windowWidth/2
+                };
+                var modalLayer2;
+
+                menu.mainMenu.show();
+                menu.mainMenu.css({
+                    width: menuSize,
                     position: "absolute",
-                    width: screenWidth,
+                    top: panelWidth/2 - menuSize/2,
+                    left: panelWidth/2 - menuSize/2
+                });
+                menu.$menuPanel.css({
+                    width: panelWidth + "px",
                     height: screenHeight,
-                    opacity: 0
-                }).appendTo(document.body).moveToTop();
-                menu.messageWindow.moveToTop().show();
-                menu.okButton.moveToTop().show();
-                menu.cancelButton.moveToTop().show();
-            });
-
-            $(document).on(isMobile ? "touchend" : "click", function(event) {
-                var target = $(event.target);
-                event.stopPropagation();
-                if(! (target.is('#menuButton') || target.is('#menuPanel') || target.is('#buttons') ||
-                    target.is('.navButton')) ) {
+                    marginLeft: - panelWidth + "px"
+                });
+                menu.buttons.find(".navButton").css({
+                    width: buttonSize + "px",
+                    marginBottom: 30 * panelWidth / 100 + "px"
+                }).show();
+                menu.buttons.css({
+                    marginLeft: - panelWidth + "px",
+                    marginTop: - screenHeight + (panelWidth/2 - menuSize/2) + menuSize * 1.4 + "px"
+                });
+                menu.messageWindow.css({
+                    top: messageWindowPosition.top,
+                    left: messageWindowPosition.left,
+                    width: windowWidth
+                });
+                menu.okButton.css({
+                    position: "absolute",
+                    top: messageWindowPosition.top + 50 * windowHeight / 100 + "px",
+                    left: messageWindowPosition.left + 15 * windowWidth / 100 + "px",
+                    width: 30 * windowWidth / 100,
+                    marginRight: 10 * windowWidth / 100 + "px"
+                });
+                menu.cancelButton.css({
+                    position: "absolute",
+                    top: messageWindowPosition.top + 50 * windowHeight / 100 + "px",
+                    left: messageWindowPosition.left + 55 * windowWidth / 100 + "px",
+                    width: 30 * windowWidth / 100 + "px"
+                });
+                //////////////////////////////////////////////////////////////////////
+                menu.mainMenu.on(isMobile ? "touchend" : "click", function(event) {
+                    event.stopPropagation();
+                    if(isShownMenu) {
+                        menu.hide();
+                    } else {
+                        menu.show();
+                    }
+                });
+                menu.backButton.on(isMobile ? "touchend" : "click", function() {
+                    if($(document.body).attr("class") == "getPuzzle") {
+                        document.location.href = localStorage.getItem('environment') + "Environment.html";
+                    } else {
+                        document.location.href = "environmentChoose.html";
+                    }
+                });
+                menu.okButton.on(isMobile ? "touchend" : "click", function(event) {
+                    event.stopPropagation();
+                    document.location.href = document.location.href;
+                    modalLayer2.remove();
+                    menu.show();
+                });
+                menu.cancelButton.on(isMobile ? "touchend" : "click", function(event) {
+                    event.stopPropagation();
+                    menu.messageWindow.hide();
+                    menu.okButton.hide();
+                    menu.cancelButton.hide();
+                    modalLayer2.remove();
+                    menu.show();
+                });
+                menu.reloadButton.on(isMobile ? "touchend" : "click", function() {
+                    modalLayer2 = $("<div></div>").css({
+                        position: "absolute",
+                        top: 0,
+                        width: screenWidth,
+                        height: screenHeight,
+                        zIndex: 150,
+                        opacity: 1
+                    }).appendTo(document.body);
+                    menu.messageWindow.show();
+                    menu.okButton.show();
+                    menu.cancelButton.show();
                     menu.hide();
-                }
-                return null;
-            });
+                });
+
+                $(document).on(isMobile ? "touchend" : "click", function(event) {
+                    var target = $(event.target);
+                    event.stopPropagation();
+                    if(! (target.is('#menuButton') || target.is('#menuPanel') || target.is('#buttons') ||
+                        target.is('.navButton')) ) {
+                        menu.hide();
+                    }
+                    return null;
+                });
+            }
+            var imgSources = {
+                messageWindow: "blue_button-555px.png",
+                okButton: "orange-button-png.png",
+                cancelButton: "orange-button-png.png",
+                mainMenu: "btn_menu.png",
+                backButton: "btn_back.png",
+                reloadButton: "btn_refresh.png"
+            };
+            loadImages(imgSources, initImagesBehavior);
         },
         show: function() {
+            menu.modalLayer1 = $("<div></div>").css({
+                position: "absolute",
+                top: 0,
+                width: screenWidth,
+                height: screenHeight,
+                zIndex: 80,
+                opacity: 1
+            }).appendTo(document.body);
             menu.$menuPanel.animate({
                 marginLeft: 0
             }, 500);
@@ -119,12 +176,13 @@ menuConfig = function() {
             menu.$menuPanel.animate({
                 marginLeft: - 20 * screenWidth / 100 + "px"
             }, 500, function() {
-                menu.$mainMenu.show();
+                menu.mainMenu.show();
             });
             menu.buttons.animate({
                 marginLeft: - 20 * screenWidth / 100 + "px"
             }, 500);
             isShownMenu = 0;
+            menu.modalLayer1.remove();
         }
     };
 
